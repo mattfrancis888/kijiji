fd3066f5861d0c3deed5c271e95e1c36dc7fdf1c
Matthew Francis <mattfrancis888@gmail.com>
2020-12-22 19:08:53 -0500

    Created form for posting ads. Responsive layout to all screens. Form currently has title and description.

--- server/client/src/components/PostAd.tsx
+++ server/client/src/components/PostAd.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import requireAuth from "./requireAuth";
+import PostAdForm from "./PostAdForm";
+export interface PostAdFormProps {
+    onSubmit(formValues: any): void;
+    //authStatus?: string | null;
+}
+
 const PostAd: React.FC<{}> = () => {
     return (
-        <div>
-            <h1>Post Ad Page</h1>
+        <div className="postAdPageContainer">
+            <h1>Post Your Ad, it's fast and easy</h1>
+            <PostAdForm />
         </div>
     );
 };
---
+++ server/client/src/components/PostAdForm.tsx
@@ -0,0 +1,149 @@
+import React, { ComponentType, useEffect } from "react";
+import {
+    Field,
+    reduxForm,
+    reset,
+    change,
+    FormErrors,
+    InjectedFormProps,
+} from "redux-form";
+//compose is used to make it easier to "organize" mapStateToProps and redux form
+import { StoreState } from "../reducers";
+import { connect } from "react-redux";
+import { PostAdFormProps } from "./PostAd";
+//Re-usable component
+export interface RegisterFormValues {
+    title: string;
+    description: string;
+}
+
+const renderError = ({ error, touched }: any) => {
+    if (touched && error) {
+        //Touched (for input) will be false at first
+        //When clicked and then clicked otuside of the input, it will be true
+        return <div className="errorText">{error}</div>;
+    }
+};
+
+const renderTextInput = ({ input, label, meta, placeHolder }: any) => {
+    //"component" property automatically passes props to argument, it has {input properties and meta properties}
+    //"label" automatically passes props to arguments
+    return (
+        <div>
+            {/* <label>{label}</label> */}
+            <input
+                className="createPostAdInputs"
+                {...input}
+                autoComplete="off"
+            />
+            {renderError(meta)}
+        </div>
+    );
+    //{..input} is shortcut for redux-form; where you take all the input from "component's" props and pass it as
+    //props to <input>
+};
+
+const renderTextArea = ({ input, label, meta, placeHolder }: any) => {
+    //"component" property automatically passes props to argument, it has {input properties and meta properties}
+    //"label" automatically passes props to arguments
+    return (
+        <div className="postAdTextAreaWrap">
+            {/* <label>{label}</label> */}
+            <textarea
+                className="createPostAdTextArea"
+                {...input}
+                autoComplete="off"
+            />
+            {renderError(meta)}
+        </div>
+    );
+    //{..input} is shortcut for redux-form; where you take all the input from "component's" props and pass it as
+    //props to <input>
+};
+
+const PostAdForm: React.FC<
+    PostAdFormProps & InjectedFormProps<{}, PostAdFormProps>
+> = (props) => {
+    const onSubmit = (formValues: any, dispatch: any) => {
+        //onSubmit's default param is any
+        //event.preventDefault() is automatically called with handleSubmit, a redux-form property
+        //form values are the values from the fields that redux-form automatiacally passes
+        //after clicking the submit button
+        //dispatch(reset("registerForm"));
+        props.onSubmit(formValues);
+
+        //dispatch(change("registerForm", "password", ""));
+    };
+
+    return (
+        <React.Fragment>
+            <form
+                className="postAdForm"
+                onSubmit={props.handleSubmit(onSubmit)}
+            >
+                <div className="postAdFieldSection">
+                    <div className="postAdFieldTitleWrap">
+                        <h1>Ad Title</h1>
+                        <h3 className="authFormFieldTitleEmailInUse">
+                            {props.authStatus}
+                        </h3>
+                    </div>
+                    <Field
+                        name="title"
+                        type="text"
+                        // label="Ad Title"
+                        component={renderTextInput}
+                    />
+                </div>
+                <div className="postAdFieldSection">
+                    <div className="postAdFieldTitleWrap">
+                        <h1>Description</h1>
+                        <h3 className="authFormFieldTitleEmailInUse">
+                            {props.authStatus}
+                        </h3>
+                    </div>
+                    <Field
+                        name="description"
+                        type="text"
+                        component={renderTextArea}
+                    />
+                </div>
+
+                <button className="postAdFormSubmit">Post Your Ad</button>
+            </form>
+        </React.Fragment>
+    );
+};
+
+const validate = (
+    formValues: RegisterFormValues
+): FormErrors<RegisterFormValues> => {
+    //MUST BE NAMED VALIDATE! Other names would be ignored by reduxForm(..)
+    const errors: FormErrors<RegisterFormValues> = {};
+    //If you return an empty object, redux form will assume everything is ok
+    if (!formValues.title) {
+        //user did not enter title, so undefined
+        errors.title = "You must enter a title";
+        //Must be the same name as field name! The "error" property in {meta} would receive this
+    }
+
+    if (!formValues.description) {
+        errors.description = "You must enter a description";
+    }
+
+    return errors;
+    //Erors is going to be passed to renderInput's meta
+};
+
+// const mapStateToProps = (state: StoreState) => {
+//     return {
+//         authStatus: state.authStatus.errorMessage,
+//     };
+// };
+
+export default connect()(
+    reduxForm<{}, PostAdFormProps>({
+        form: "postAdForm",
+        validate,
+    })(PostAdForm)
+);
