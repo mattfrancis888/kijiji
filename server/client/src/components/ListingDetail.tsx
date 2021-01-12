b2ce9967c5cdbb24f49671a6eb46fd4c354bdfb4
Matthew Francis <mattfrancis888@gmail.com>
2021-01-12 13:20:47 -0500

    Show more text is functional. Added a categories section that shows what categories the listing resides in.

--- server/client/src/components/ListingDetail.tsx
+++ server/client/src/components/ListingDetail.tsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import history from "../browserHistory";
 import { connect } from "react-redux";
 import { StoreState } from "../reducers";
 import { Listing as ListingType } from "../actions";
-import moment from "moment";
 import postAdListingImagePlaceHolder from "../img/postAdListingImagePlaceHolder.png";
 const ListingDetail: React.FC<ListingType> = ({
     listing_id,
@@ -17,8 +16,13 @@ const ListingDetail: React.FC<ListingType> = ({
     city,
     street,
 }) => {
+    const [showMore, setShowMore] = useState(false);
+
     return (
         <React.Fragment>
+            <div className="listingDetailCategories">
+                <h3>{`Ontario > Toronto`}</h3>
+            </div>
             <div className="listingDetailAndContactContainer">
                 <div className="listingDetailContainer">
                     <div className="listingDetailImageContainer">
@@ -32,13 +36,31 @@ const ListingDetail: React.FC<ListingType> = ({
                     </div>
                     <div className="listingDetailDescriptionContainer">
                         <div className="listingDescriptionTextWrap">
+                            <h3 className="listingDetailPostedOn">
+                                Posted On:{" "}
+                            </h3>
+                            <h3 className="locationOfPost">Toronto</h3>
                             <h3 className="listingDetailDescriptionTitle">
                                 Description
                             </h3>
+
                             <p>Hello text</p>
                         </div>
-                        <div className="listingDetailDescriptionFade"></div>
-                        <h3 className="listingDetailShowMoreText">Show More</h3>
+                        <div
+                            className={
+                                showMore ? "" : "listingDetailDescriptionFade"
+                            }
+                        ></div>
+                        <h3
+                            className={
+                                showMore
+                                    ? "listingDetailShowMoreTextHide"
+                                    : "listingDetailShowMoreText"
+                            }
+                            onClick={() => setShowMore(true)}
+                        >
+                            Show More
+                        </h3>
                     </div>
                 </div>
                 <div className="listingDetailContactContainer">
